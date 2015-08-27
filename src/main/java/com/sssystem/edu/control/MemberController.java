baa3f81704eba44ba0dc624026eba3c9da72f9d7
rladydtjd031 <rladydtjd031@hanmail.net>
2015-08-27 16:20:07 +0900

    test

--- src/main/java/com/sssystem/edu/control/MemberController.java
+++ src/main/java/com/sssystem/edu/control/MemberController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
+	
+	@RequestMapping("asdsa")
+	public String lpggg(){
+		return "asdasd";
+	}
 
 	@RequestMapping("/member/login")
 	public String login(){
